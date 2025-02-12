85b3cbe7c7890e20e5e59cb371edf4ae2c68def3
ulanovvs <ulanovvs@sgaz.pro>
2025-02-12 20:56:46 +0300

    Init project

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import words
+from apscheduler.schedulers.background import BackgroundScheduler
+import requests
 
 app = FastAPI(title='Vocabulary API')
 
@@ -17,6 +19,24 @@ app.add_middleware(
 )
 
 app.include_router(words.router)
+
+# Функция для пинга сервера
+def ping_server():
+    try:
+        response = requests.get("https://site-front-5j5u.onrender.com")
+        print(f"Ping successful. Status code: {response.status_code}")
+    except Exception as e:
+        print(f"Ping failed. Error: {str(e)}")
+
+# Настройка и запуск планировщика
+scheduler = BackgroundScheduler()
+scheduler.add_job(ping_server, 'interval', minutes=5)
+scheduler.start()
+
+# Добавьте эндпоинт для проверки работоспособности
+@app.get("/api/health-check")
+async def health_check():
+    return {"status": "ok"}
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='127.0.0.1', port=8000, reload=True)
